db0be0d08a55e53ca31fc44d125fe202ea6ef0de
Tyler van der Hoeven <hi@tyvdh.com>
2022-08-10 21:54:43 -0400

    reorder the docs content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,20 +1,23 @@
 // @ts-check
 
-const lightCodeTheme = require("prism-react-renderer/themes/nightOwl");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+// const lightCodeTheme = require("prism-react-renderer/themes/nightOwl");
+// const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Stellar Documentation",
-  tagline:
-    "Stellar is a self-serve distributed ledger that you can use as a backend to power all kinds of apps and services",
+  tagline: "Stellar is a self-serve distributed ledger that you can use as a backend to power all kinds of apps and services",
+  url: "https://developers.stellar.org",
+  baseUrl: "/",
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon-96x96.png",
   organizationName: "stellar",
   projectName: "stellar-docs",
-  url: "https://developers.stellar.org",
-  baseUrl: "/",
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
   plugins: ["docusaurus-plugin-sass"],
   presets: [
     [
@@ -23,10 +26,10 @@ const config = {
       ({
         blog: false,
         docs: {
-          routeBasePath: "/",
           showLastUpdateTime: true,
           breadcrumbs: true,
-          remarkPlugins: [require("mdx-mermaid")],
+          routeBasePath: "/",
+          // remarkPlugins: [require("mdx-mermaid")],
           sidebarPath: require.resolve("./sidebars.js"),
           editUrl: "https://github.com/stellar/stellar-docs/tree/main/",
         },
@@ -37,115 +40,122 @@ const config = {
     ],
   ],
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      docs: { sidebar: { autoCollapseCategories: true } },
-      navbar: {
-        hideOnScroll: true,
-        logo: {
-          width: 100,
-          src: "img/stellar-logo.svg",
-          srcDark: "img/stellar-logo-dark.svg",
-          href: "/docs",
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  ({
+    docs: { 
+      sidebar: { 
+        autoCollapseCategories: false 
+      } 
+    },
+    navbar: {
+      logo: {
+        width: 100,
+        src: "img/stellar-logo.svg",
+        srcDark: "img/stellar-logo-dark.svg",
+        href: "/",
+      },
+      items: [
+        {
+          to: '/',
+          label: 'Docs',
+          position: 'left'
         },
-        items: [
-          {
-            href: "https://developers.stellar.org/api",
-            label: "API",
-            position: "right",
-          },
-          {
-            href: "https://github.com/stellar/stellar-docs",
-            label: "GitHub",
-            position: "right",
-          },
-          {
-            type: "search",
-            position: "right",
-          },
-        ],
-      },
-      // TODO: update after site is deployed to production
-      algolia: {
-        appId: "testingPJZPDNR7VG",
-        apiKey: "1f36375ebbc70d65c5b8165ecf52a1f2",
-        indexName: "crawler_Docusaurus",
-      },
-      footer: {
-        links: [
-          {
-            title: "Resources",
-            items: [
-              {
-                label: "Get Started",
-                href: "https://www.stellar.org/start",
-              },
-              {
-                label: "Learn",
-                href: "https://www.stellar.org/learn/intro-to-stellar",
-              },
-              {
-                label: "Case Studies",
-                href: "https://www.stellar.org/case-studies",
-              },
-              {
-                label: "Stellar Quest",
-                href: "https://quest.stellar.org/",
-              },
-            ],
-          },
-          {
-            title: "Tools",
-            items: [
-              {
-                label: "Account Viewer",
-                href: "https://accountviewer.stellar.org/",
-              },
-              {
-                label: "Laboratory",
-                href: "https://laboratory.stellar.org/",
-              },
-              {
-                label: "Dashboard",
-                href: "https://dashboard.stellar.org/",
-              },
-              {
-                label: "Ledger Explorer",
-                href: "https://stellar.expert/explorer/public",
-              },
-              { label: "All Tools", href: "https://www.stellar.org/tools" },
-            ],
-          },
-          {
-            title: "Developers",
-            items: [
-              {
-                label: "Developer Resources",
-                href: "https://www.stellar.org/developers-legacy",
-              },
-              {
-                label: "Status",
-                href: "https://status.stellar.org/",
-              },
-              {
-                label: "Technical Papers",
-                to: "https://www.stellar.org/developers/guides/concepts/scp.html",
-              },
-              {
-                label: "Developer Blog",
-                href: "https://www.stellar.org/developers-blog",
-              },
-            ],
-          },
-        ],
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        additionalLanguages: ["java"],
-      },
-      tableOfContents: { maxHeadingLevel: 2 },
-    }),
+        {
+          href: "https://developers.stellar.org/api",
+          label: "API",
+          position: "right",
+        },
+        {
+          href: "https://github.com/stellar/stellar-docs",
+          label: "GitHub",
+          position: "right",
+        },
+        {
+          type: "search",
+          position: "right",
+        },
+      ],
+    },
+    // TODO: update after site is deployed to production
+    algolia: {
+      appId: "testingPJZPDNR7VG",
+      apiKey: "1f36375ebbc70d65c5b8165ecf52a1f2",
+      indexName: "crawler_Docusaurus",
+    },
+    footer: {
+      links: [
+        {
+          title: "Resources",
+          items: [
+            {
+              label: "Get Started",
+              href: "https://www.stellar.org/start",
+            },
+            {
+              label: "Learn",
+              href: "https://www.stellar.org/learn/intro-to-stellar",
+            },
+            {
+              label: "Case Studies",
+              href: "https://www.stellar.org/case-studies",
+            },
+            {
+              label: "Stellar Quest",
+              href: "https://quest.stellar.org/",
+            },
+          ],
+        },
+        {
+          title: "Tools",
+          items: [
+            {
+              label: "Account Viewer",
+              href: "https://accountviewer.stellar.org/",
+            },
+            {
+              label: "Laboratory",
+              href: "https://laboratory.stellar.org/",
+            },
+            {
+              label: "Dashboard",
+              href: "https://dashboard.stellar.org/",
+            },
+            {
+              label: "Ledger Explorer",
+              href: "https://stellar.expert/explorer/public",
+            },
+            { label: "All Tools", href: "https://www.stellar.org/tools" },
+          ],
+        },
+        {
+          title: "Developers",
+          items: [
+            {
+              label: "Developer Resources",
+              href: "https://www.stellar.org/developers-legacy",
+            },
+            {
+              label: "Status",
+              href: "https://status.stellar.org/",
+            },
+            {
+              label: "Technical Papers",
+              to: "https://www.stellar.org/developers/guides/concepts/scp.html",
+            },
+            {
+              label: "Developer Blog",
+              href: "https://www.stellar.org/developers-blog",
+            },
+          ],
+        },
+      ],
+    },
+    prism: {
+      // theme: lightCodeTheme,
+      // darkTheme: darkCodeTheme,
+      // additionalLanguages: ["java"],
+    },
+  }),
 };
 
 module.exports = config;
