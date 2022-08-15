aee1d9a2178acd5cbdcdee69eec99112449e6def
Tyler van der Hoeven <hi@tyvdh.com>
2022-08-15 15:28:52 -0400

    remove ga comments, it works

--- src/analytics-module.js
+++ src/analytics-module.js
@@ -8,11 +8,7 @@ module.exports = function (context, options) {
           {
             tagName: 'script',
             innerHTML: `
-              console.log('hello world');
-
               if (typeof window.ga === "function") {
-                console.log('it worked')
-
                 window.ga("require", "linker");
                 window.ga("linker:autolink", ["www.stellar.org", "stellar.org"]);
               }
