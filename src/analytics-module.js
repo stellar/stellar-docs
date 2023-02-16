e04f83130456f4996601087d57832b42d32a6177
Tyler van der Hoeven <hi@tyvdh.com>
2023-02-16 13:18:18 -0500

    add new gtm

--- src/analytics-module.js
+++ src/analytics-module.js
@@ -3,6 +3,18 @@ module.exports = () => ({
 
   injectHtmlTags() {
     return {
+      headTags: [
+        {
+          tagName: 'script',
+          innerHTML: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NDKZ834');
+          `
+        }
+      ],
       preBodyTags: [
         {
           tagName: 'script',
@@ -13,6 +25,13 @@ module.exports = () => ({
             }
           `,
         },
+        {
+          tagName: 'noscript',
+          innerHTML: `
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NDKZ834"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>
+          `
+        }
       ],
     };
   },
