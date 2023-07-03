8868331181626a0b1126855fd470f82061d7a429
Elliot Voris <elliot@stellar.org>
2023-07-03 13:27:06 -0500

    Changind dropdown menu text `API` -> `APIs`
    Signed-off-by: Elliot Voris <elliot@stellar.org>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -131,7 +131,7 @@ const config = {
           },
           {
             type: "dropdown",
-            label: "API",
+            label: "APIs",
             position: "left",
             items: [
               {
