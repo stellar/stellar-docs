e06b1f606e035aba73c00e58169bfff0882c52c0
Elliot Voris <elliot@stellar.org>
2024-04-16 12:42:18 -0500

    delete mispelled constants file

--- src/contants.ts
+++
@@ -1,5 +0,0 @@
-export const FUTURENET_DETAILS = {
-  network: "FUTURENET",
-  networkUrl: "https://horizon-futurenet.stellar.org",
-  networkPassphrase: "Test SDF Future Network ; October 2022",
-};
