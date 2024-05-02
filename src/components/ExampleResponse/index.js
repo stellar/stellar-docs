3dcba850cdabfe02be83949e29da441d665c6859
Elliot Voris <elliot@stellar.org>
2024-05-02 14:55:29 -0500

    another thing to log

--- src/components/ExampleResponse/index.js
+++ src/components/ExampleResponse/index.js
@@ -4,6 +4,7 @@ import clsx from "clsx";
 import styles from "./styles.module.scss";
 
 export const ExampleResponse = ({ children, title = "Example" }) => {
+  console.log('ExampleResponse children', children)
   const codeElement = children.props.children;
 
   return React.cloneElement(codeElement, {
