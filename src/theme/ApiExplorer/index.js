80936d259e47e32cc2a7c1bfc31866518d5df3fd
Elliot Voris <elliot@stellar.org>
2024-05-02 10:17:41 -0500

    rename api demo swizzled component to reflect upstream changes

--- src/theme/ApiExplorer/index.js
+++ src/theme/ApiExplorer/index.js
@@ -1,17 +1,18 @@
-import React from "react";
-import ApiDemoPanel from "@theme-original/ApiDemoPanel";
+import React from 'react';
+import ApiExplorer from '@theme-original/ApiExplorer';
 import Admonition from "@theme/Admonition";
 import Link from "@docusaurus/Link";
 
-export default function ApiDemoPanelWrapper(props) {
+export default function ApiExplorerWrapper(props) {
+  console.log('api explorer wrapper props', props)
   return (
     <>
-      <ApiDemoPanel {...props} />
+      <ApiExplorer {...props} />
 
       {props?.item?.["x-supports-streaming"] && (
         <Admonition type="tip" title="Supports Streaming">
           This endpoint supports streaming. To read more about this, visit the
-          <Link className="padding-horiz--xs" to="/api/introduction/streaming">
+          <Link className="padding-horiz--xs" to="/network/horizon/api-reference/structure/streaming">
             streaming section.
           </Link>
         </Admonition>
