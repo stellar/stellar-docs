84e16962f51eae2046d171e6ba9ed54cf4ea9dc1
Elliot Voris <elliot@stellar.org>
2024-05-02 16:30:59 -0500

    re-swizzle docsidebar component

---
+++ src/theme/DocSidebar/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import DocSidebar from '@theme-original/DocSidebar';
+import type DocSidebarType from '@theme/DocSidebar';
+import type {WrapperProps} from '@docusaurus/types';
+
+type Props = WrapperProps<typeof DocSidebarType>;
+
+export default function DocSidebarWrapper(props: Props): JSX.Element {
+  console.log('DocsSidebarWrapper props', props)
+  let newProps
+
+  // For all `/network` sidebars, remove the parent category from the sidebar
+  if (props.path.startsWith('/network'))  {
+    newProps = {
+      ...props,
+      sidebar: props.sidebar[0]?.items ?? undefined
+    }
+  }
+  // For all other sidebars, pass the default props
+  else {
+    newProps = props
+  }
+
+  return (
+    <>
+      <DocSidebar {...newProps} />
+    </>
+  );
+}
