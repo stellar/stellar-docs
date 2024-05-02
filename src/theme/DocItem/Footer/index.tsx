ed6b1747225ff6271a3edcc7f0d78ed3abb5450e
Elliot Voris <elliot@stellar.org>
2024-05-02 15:49:19 -0500

    re-swizzle doc footer component (use typescript, too)

---
+++ src/theme/DocItem/Footer/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Footer from '@theme-original/DocItem/Footer';
+import { useDoc } from "@docusaurus/theme-common/internal";
+import ReaderFeedback from '@site/src/components/ReaderFeedback';
+import DocCardList from '@theme/DocCardList'
+import type FooterType from '@theme/DocItem/Footer';
+import type { WrapperProps } from '@docusaurus/types';
+import clsx from 'clsx';
+
+type Props = WrapperProps<typeof FooterType>;
+
+export default function FooterWrapper(props: Props): JSX.Element {
+  const { metadata } = useDoc();
+  console.log('FooterWrapper metadata', metadata)
+
+  const canDisplayDocCardsOnGuide = metadata.permalink?.startsWith('/docs/smart-contracts/guides');
+  return (
+    <>
+      {canDisplayDocCardsOnGuide &&
+        <div className={clsx(metadata.permalink === '/docs/smart-contracts/guides/' ? 'margin-top--lg' : 'margin-top--xl')}>
+          {metadata.permalink !== '/docs/smart-contracts/guides/' && <h3>Guides in this category:</h3>}
+          <DocCardList />
+        </div>
+      }
+      <ReaderFeedback pageId={metadata.id} />
+      <Footer {...props} />
+    </>
+  );
+}
