71d7b4128d7ae27f98b05398983fdc3369bc50b8
Elliot Voris <elliot@stellar.org>
2024-06-14 13:28:09 -0500

    we don't need to check for core-node in swizzled ApiItem component

--- src/theme/ApiItem/index.js
+++ src/theme/ApiItem/index.js
@@ -7,7 +7,6 @@ export default function ApiItemWrapper(props) {
     props.location?.pathname?.includes('admin-guide')
     || props.location?.pathname?.startsWith('/network/hubble')
     || props.location?.pathname?.startsWith('/network/soroban-rpc')
-    || props.location?.pathname?.startsWith('/network/core-node')
     || props.location?.pathname === '/network/requirements'
     || props.location?.pathname === '/network/horizon/horizon-providers'
   ) {
