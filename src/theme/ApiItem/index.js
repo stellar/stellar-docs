18ca97f4e429168c2d91cce48b6b6ea67ff69e34
Aditya Vyas <aditya.vyas@stellar.org>
2024-06-24 16:25:55 -0400

    Update index.js

--- src/theme/ApiItem/index.js
+++ src/theme/ApiItem/index.js
@@ -4,7 +4,7 @@ import DocItem from '@theme-original/DocItem';
 
 export default function ApiItemWrapper(props) {
   if (
-    props.location?.pathname?.includes('api-reference')
+      props.location?.pathname?.includes('api-reference') && !props.location?.pathname.includes('error-handling')
   ) {
     return (
       <>
