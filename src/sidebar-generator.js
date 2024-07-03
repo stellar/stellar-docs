e3a1bb4eda8b12d901f9175af85d739144c60979
Elliot Voris <elliot@stellar.org>
2024-07-03 15:21:58 -0500

    adjust sidebar generator for main docs plugin

--- src/sidebar-generator.js
+++ src/sidebar-generator.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
 
 module.exports = async ({ defaultSidebarItemsGenerator, ...args }) => {
   const docs = args.docs.filter(
@@ -47,7 +47,7 @@ module.exports = async ({ defaultSidebarItemsGenerator, ...args }) => {
     const sidebarPath = path.join(
       args.version.contentPath,
       args.item.dirName,
-      "./api-reference/resources/sidebar.js",
+      "./api-reference/resources/sidebar.ts",
     );
 
     if (resources && fs.existsSync(sidebarPath)) {
