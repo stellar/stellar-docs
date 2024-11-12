17404c76cce67285a5cb05e4e7a8b6da532f6760
Nando Vieira <me@fnando.com>
2024-11-12 08:10:50 -0800

    Pull stellar-cli docs from latest released version. (#1068)

--- scripts/stellar_cli.mjs
+++ scripts/stellar_cli.mjs
@@ -1,23 +1,26 @@
 import fs from "fs-extra";
-import path from 'path';
-import { execSync } from 'child_process';
+import path from "path";
+import { execSync } from "child_process";
 
-const repoUrl = 'https://github.com/stellar/stellar-cli.git';
-const localRepoPath = './stellar-cli-repo';
+const repoUrl = "https://github.com/stellar/stellar-cli.git";
+const localRepoPath = "./stellar-cli-repo";
 
 // Remove the existing repo if it exists
 if (fs.existsSync(localRepoPath)) {
-  console.log('Removing existing repository...');
+  console.log("Removing existing repository...");
   fs.removeSync(localRepoPath);
 }
 
 // Perform a shallow clone of the repository
-console.log('Cloning repository...');
-execSync(`git clone --depth 1 ${repoUrl} ${localRepoPath}`);
+console.log("Cloning repository...");
+execSync(`git clone ${repoUrl} ${localRepoPath}`);
+execSync(
+  `cd ${localRepoPath} && git checkout --quiet $(git tag | grep -v -E 'rc|preview' | tail -n1)`,
+);
 
 // Copy FULL_HELP_DOCS.md
-const fullHelpDocsPath = path.join(localRepoPath, 'FULL_HELP_DOCS.md');
-const fullHelpDocsContent = fs.readFileSync(fullHelpDocsPath, 'utf8');
+const fullHelpDocsPath = path.join(localRepoPath, "FULL_HELP_DOCS.md");
+const fullHelpDocsContent = fs.readFileSync(fullHelpDocsPath, "utf8");
 
 const modifiedContent = `---
 sidebar_position: 30
@@ -27,14 +30,15 @@ description: This document contains the help content for the stellar command-lin
 ${fullHelpDocsContent}
 `;
 
-fs.writeFileSync("docs/tools/developer-tools/cli/stellar-cli.mdx", modifiedContent);
-
-fs.cpSync(
-  path.join(localRepoPath, 'cookbook'),
-  'docs/build/guides/cli',
-  { recursive: true },
+fs.writeFileSync(
+  "docs/tools/developer-tools/cli/stellar-cli.mdx",
+  modifiedContent,
 );
 
-execSync('yarn format:mdx');
+fs.cpSync(path.join(localRepoPath, "cookbook"), "docs/build/guides/cli", {
+  recursive: true,
+});
 
-console.log('All files processed successfully.');
+execSync("yarn format:mdx");
+
+console.log("All files processed successfully.");
