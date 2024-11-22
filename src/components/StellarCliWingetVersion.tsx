f6b2b1060288d45f824189bd035f42e7366588f9
Nando Vieira <me@fnando.com>
2024-11-21 22:19:53 -0800

    Use just numbers.

--- src/components/StellarCliWingetVersion.tsx
+++ src/components/StellarCliWingetVersion.tsx
@@ -3,7 +3,7 @@ import CodeBlock from "@theme/CodeBlock";
 import { latestVersion } from "@site/src/helpers/stellarCli";
 
 export function StellarCliWingetVersion() {
-  const command = `winget install --id Stellar.StellarCLI --version v${latestVersion}`;
+  const command = `winget install --id Stellar.StellarCLI --version ${latestVersion}`;
 
   return <CodeBlock language="powershell">{command}</CodeBlock>;
 }
