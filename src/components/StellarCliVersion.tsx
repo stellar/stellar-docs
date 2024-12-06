5c461b0c03c747853c04fad39f9c49ff7842ff5c
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-12-07 08:39:25 +1000

    Fix stellar-cli version (#1116)

--- src/components/StellarCliVersion.tsx
+++ src/components/StellarCliVersion.tsx
@@ -3,7 +3,7 @@ import CodeBlock from "@theme/CodeBlock";
 import { latestVersion } from "@site/src/helpers/stellarCli";
 
 export function StellarCliVersion() {
-  const command = `cargo install --locked stellar-cli --version v${latestVersion} --features opt`;
+  const command = `cargo install --locked stellar-cli@${latestVersion} --features opt`;
 
   return <CodeBlock language="sh">{command}</CodeBlock>;
 }
