ad8818cd0968168c993df8b2de230676393a03e3
Elliot Voris <elliot@stellar.org>
2024-12-09 13:18:30 -0600

    add another crowdin fix to the script

--- scripts/fix_md_comments.sh
+++ scripts/fix_md_comments.sh
@@ -4,3 +4,4 @@ perl -i -pe's/{\/_/{\/\*/' i18n/es/docusaurus-plugin-content-pages/docs/learn/in
 perl -i -pe's/_\/}/\*\/}/' i18n/es/docusaurus-plugin-content-pages/docs/learn/interactive/dapps/challenges/*
 perl -i -pe's/<(http.*)>/[\1](\1)/' i18n/es/docusaurus-plugin-content-pages/docs/learn/interactive/dapps/challenges/*
 perl -i -pe's/\s\{/ \\{/g' i18n/es/docusaurus-plugin-content-docs/current/learn/fundamentals/transactions/list-of-operations.mdx
+perl -i -pe's/<\s(.*\.mdx)>/\1/' i18n/es/docusaurus-plugin-content-docs-ap/**/admin-guide/component/observer/observer.mdx
