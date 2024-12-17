da65e45c5fd75dbf5f0dc083bab180d6d721b8fc
Bri Wylde <92327786+briwylde08@users.noreply.github.com>
2024-12-17 10:16:50 -0700

    Remove Dapps Challenge / Soroban Quest from docs (#1132)
    * remove old games from docs
    
    * add soroban quest back to footer
    
    * remove dapps challenge pages
    
    * remove custom react components for dapps challenges
    
    * add back modified fca00c description page
    
    * extra period
    
    ---------
    
    Co-authored-by: Elliot Voris <elliot@stellar.org>

--- src/utils/get-active-challenge.ts
+++
@@ -1,6 +0,0 @@
-import { ChallengeInfo } from "../interfaces/challenge";
-
-export const getActiveChallenge = (
-  data: ChallengeInfo[],
-  challengeId: number,
-): ChallengeInfo | undefined => data.find((item: ChallengeInfo) => item.id === challengeId);
