331befcb6a9bdfc97ab54bf120576f873284a498
Elliot Voris <elliot@stellar.org>
2024-12-20 14:30:09 -0600

    add a check to ensure only tables are rendered as tables

--- src/components/Homepage/PathfindingTable/index.tsx
+++ src/components/Homepage/PathfindingTable/index.tsx
@@ -5,19 +5,23 @@ import styles from './styles.module.css'
 export default function PathfindingTable({ children }) {
   return (
     React.Children.map(children, child => {
-      const [thead, tbody] = child.props.children
-      const rows = tbody.props.children
+      if (child.type === 'table') {
+        const [thead, tbody] = child.props.children
+        const rows = tbody.props.children
 
-      return (
-        <table className={styles.PathfindingTable}>
-          <thead>
-            <th colSpan={3} {...thead.props.children.props.children[0].props}></th>
-          </thead>
-          <tbody>
-            {React.Children.map(rows, row => <PathfindingRow row={row} />)}
-          </tbody>
-        </table>
-      )
+        return (
+          <table className={styles.PathfindingTable}>
+            <thead>
+              <th colSpan={3} {...thead.props.children.props.children[0].props}></th>
+            </thead>
+            <tbody>
+              {React.Children.map(rows, row => <PathfindingRow row={row} />)}
+            </tbody>
+          </table>
+        )
+      } else {
+        return child
+      }
     })
   )
 }
