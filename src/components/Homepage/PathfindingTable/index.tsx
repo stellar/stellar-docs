cf0734438a4cde4baf7cfc50264dbc32f590932b
Elliot Voris <elliot@stellar.org>
2024-12-19 18:10:34 -0600

    add a pathfinding table custom component

---
+++ src/components/Homepage/PathfindingTable/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PathfindingRow from './PathfindingRow';
+import styles from './styles.module.css'
+
+export default function PathfindingTable({ children }) {
+  return (
+    React.Children.map(children, child => {
+      const [thead, tbody] = child.props.children
+      const rows = tbody.props.children
+
+      return (
+        <table className={styles.PathfindingTable}>
+          <thead>
+            <th colSpan={3} {...thead.props.children.props.children[0].props}></th>
+          </thead>
+          <tbody>
+            {React.Children.map(rows, row => <PathfindingRow row={row} />)}
+          </tbody>
+        </table>
+      )
+    })
+  )
+}
