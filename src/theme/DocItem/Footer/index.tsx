babbcd9dcab9a2e6c845cf848b98a0f83ecad322
Elliot Voris <elliot@stellar.org>
2025-02-11 10:43:35 -0600

    re-swizzle some theme components
    this removes the changes and customizations for now, but that's ok
    because they were failing to build. we'll add them back in before
    we merge this PR

--- src/theme/DocItem/Footer/index.tsx
+++ src/theme/DocItem/Footer/index.tsx
@@ -1,34 +1,13 @@
-import React from 'react';
+import React, {type ReactNode} from 'react';
 import Footer from '@theme-original/DocItem/Footer';
-import { useDoc } from "@docusaurus/theme-common/internal";
-import ReaderFeedback from '@site/src/components/ReaderFeedback';
-import DocCardList from '@theme/DocCardList';
 import type FooterType from '@theme/DocItem/Footer';
-import type { WrapperProps } from '@docusaurus/types';
-import clsx from 'clsx';
-import Translate from '@docusaurus/Translate';
+import type {WrapperProps} from '@docusaurus/types';
 
 type Props = WrapperProps<typeof FooterType>;
 
-export default function FooterWrapper(props: Props): JSX.Element {
-  const { metadata } = useDoc();
-
-  const canDisplayDocCardsOnGuide = metadata.permalink?.includes('/docs/build/guides');
-  const isMainGuidesPage = metadata.id === 'build/guides/README'
+export default function FooterWrapper(props: Props): ReactNode {
   return (
     <>
-      {canDisplayDocCardsOnGuide &&
-        <div className={clsx(isMainGuidesPage ? 'margin-top--lg' : 'margin-top--xl')}>
-          {!isMainGuidesPage && <h3>
-            <Translate
-              id='components.HowToGuides.GuidesInCategory'
-              description='The h3 title do display the other related how-to guides'
-            >Guides in this category:</Translate>
-          </h3>}
-          <DocCardList />
-        </div>
-      }
-      <ReaderFeedback pageId={metadata.id} />
       <Footer {...props} />
     </>
   );
