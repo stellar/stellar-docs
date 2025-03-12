7f05193c83e06896125ecd1248145934ab4e2b24
Gleb <gleb@stellar.org>
2025-03-12 16:52:05 -0700

    Update install guide (#1414)

--- src/components/StellarCliGitHubVersion.tsx
+++ src/components/StellarCliGitHubVersion.tsx
@@ -3,7 +3,7 @@ import CodeBlock from "@theme/CodeBlock";
 import { latestVersion } from "@site/src/helpers/stellarCli";
 
 export function StellarCliGitHubVersion() {
-  const command = `uses: stellar/stellar-cli@${latestVersion}`;
+  const command = `uses: stellar/stellar-cli@v${latestVersion}`;
 
   return <CodeBlock language="yaml">{command}</CodeBlock>;
 }
