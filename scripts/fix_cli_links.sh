b3ca1c726c0f22b48c060e4b2ba14f9e8827122d
Elliot <elliot@stellar.org>
2025-10-01 11:24:37 -0500

    remove link checking for translated cli cookbook files
    we're running this command _before_ the source files are uploaded
    to crowdin, anyway. so, everything "should" already have been fixed
    by the time the translations land in the repo (in theory)

--- scripts/fix_cli_links.sh
+++ scripts/fix_cli_links.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
 
 perl -i -pe's/\.\.\/rpc(.*\.mdx)/..\/..\/..\/build\/guides\/rpc\1/g' docs/tools/cli/cookbook/*.mdx
-perl -i -pe's/\.\.\/rpc(.*\.mdx)/..\/..\/..\/build\/guides\/rpc\1/g' i18n/es/docusaurus-plugin-content-docs/current/tools/cli/cookbook/*.mdx
