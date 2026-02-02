ec6570a9f193e9487710a513f70e13dedad0ccc8
john.xlm <60260750+JFWooten4@users.noreply.github.com>
2026-02-02 08:11:06 -0500

    TSX explicit prop/return types This defers to the inline styling from #2188, but keeps it as a flat approach like all the other scripts there, as I don't envision this component growing a whole ton. There is something to be said about video implementations going forward on something like Livepeer, but I'd imagine that'd be something different—like the DriveVideo.tsx.

--- src/components/YouTube.tsx
+++ src/components/YouTube.tsx
@@ -4,34 +4,33 @@ type YouTubeVid = {
   ID: string;
 };
 
-const containerStyle: CSSProperties = {
-  position: "relative",
-  width: "100%",
-  paddingBottom: "56.25%", // Maintain 16:9 ratio
-  height: 0,
-  marginBottom: "23px",
+export function YouTube({ ID }: { ID: string; }): ReactNode {
+  return (
+    <div style={{
+      position: "relative",
+      width: "100%",
+      paddingBottom: "56.25%", // 16:9 aspect ratio
+      height: 0,
+      marginBottom: "23px",
+    }}>
+      <iframe
+        src={`https://www.youtube-nocookie.com/embed/${ID}?rel=0&modestbranding=1`}
+        title="Informational explainer"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; picture-in-picture; gyroscope" // screen tilt
+        allowFullScreen
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          border: "0px",
+          borderRadius: "25pt",
+        }}
+      />
+    </div>
+  );
 };
-
-const iframeStyle: CSSProperties = {
-  position: "absolute",
-  top: 0,
-  left: 0,
-  width: "100%",
-  height: "100%",
-  borderRadius: "25pt",
-};
-
-const YouTube = ({ ID }: YouTubeVid): JSX.Element => (
-  <div style={containerStyle}>
-    <iframe
-      src={`https://www.youtube-nocookie.com/embed/${ID}?controls=0&rel=0&modestbranding=1`}
-      title="Informational explainer"
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; picture-in-picture; gyroscope" // screen tilt
-      allowFullScreen
-      style={iframeStyle}
-    />
-  </div>
-);
 
 export default YouTube;
