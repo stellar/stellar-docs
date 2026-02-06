aa07f1d2ff1177e6efa2f3ffb8c1ce4eb4655ba3
john.xlm <60260750+JFWooten4@users.noreply.github.com>
2026-02-06 15:04:51 -0500

    script rendering (fix build)

--- src/components/LanguageSpecific.tsx
+++ src/components/LanguageSpecific.tsx
@@ -2,51 +2,51 @@ import React from "react";
 import BrowserOnly from '@docusaurus/BrowserOnly';
 
 type LanguageProps = {
-    kt?: JSX.Element;
-    ts?: JSX.Element;
-    flutter?: JSX.Element;
-    swift?: JSX.Element;
-    fallback?: JSX.Element;
+  kt?: JSX.Element;
+  ts?: JSX.Element;
+  flutter?: JSX.Element;
+  swift?: JSX.Element;
+  fallback?: JSX.Element;
 };
 
 export const walletDefaultLang = "ts";
 
 export const LanguageSpecific: React.FC<LanguageProps> = (props) => (
-        <BrowserOnly fallback={props.fallback || getToShow(props, null)}>
-            {() => getToShow(props, getCookie())}
-        </BrowserOnly>
-    );
+    <BrowserOnly fallback={props.fallback || getToShow(props, null)}>
+      {() => getToShow(props, getCookie())}
+    </BrowserOnly>
+  );
 
 const getToShow = (props: LanguageProps, cookie: String) => {
-    let toShow = <></>;
+  let toShow = <></>;
 
-    if (cookie == null) {
-        cookie = walletDefaultLang;
-    }
+  if (cookie == null) {
+    cookie = walletDefaultLang;
+  }
 
-    if (cookie == "kt") {
-        toShow = props.kt || <></>;
-    } else if (cookie == "ts") {
-        toShow = props.ts || <></>;
-    } else if (cookie == "dart") {
-        toShow = props.flutter || <></>;
-    } else if (cookie == "swift") {
-        toShow = props.swift || <></>;
-    }
+  if (cookie == "kt") {
+    toShow = props.kt || <></>;
+  } else if (cookie == "ts") {
+    toShow = props.ts || <></>;
+  } else if (cookie == "dart") {
+    toShow = props.flutter || <></>;
+  } else if (cookie == "swift") {
+    toShow = props.swift || <></>;
+  }
 
-    return toShow;
+  return toShow;
 };
 
 export function getCookie() {
-    const name = "selected_language=";
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        const ind = ca[i].indexOf(name);
-        if (ind != -1) {
-            const val = ca[i].indexOf("=");
-            return ca[i].substring(val + 1, ca[i].length);
-        }
+  const name = "selected_language=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    const ind = ca[i].indexOf(name);
+    if (ind != -1) {
+      const val = ca[i].indexOf("=");
+      return ca[i].substring(val + 1, ca[i].length);
     }
-    return null;
+  }
+  return null;
 }
--- src/components/WalletCodeExample.tsx
+++ src/components/WalletCodeExample.tsx
@@ -15,87 +15,87 @@ const SHOW_ALL = false;
 const WALLET_LANGS = ["kt", "ts", "dart", "swift"];
 
 type WalletCodeExampleProps = {
-    children: React.ReactElement;
+  children: React.ReactElement;
 };
 
 export const WalletCodeExample: React.FC<WalletCodeExampleProps> = ({children}) => <BrowserOnly fallback={getTabs(children, walletDefaultLang)}>
-        {() => getTabs(children, getCookie())}
-    </BrowserOnly>;
+    {() => getTabs(children, getCookie())}
+  </BrowserOnly>;
 
 const getTabs = (children: React.ReactElement, targetLanguage: string) => {
-    const defaultVal = CODE_LANGS[targetLanguage];
+  const defaultVal = CODE_LANGS[targetLanguage];
 
-    const tabs = React.Children.map(children, (child, index) => {
-        const codeProps = child.props.children.props;
-        const {className = ''} = codeProps;
+  const tabs = React.Children.map(children, (child, index) => {
+    const codeProps = child.props.children.props;
+    const {className = ''} = codeProps;
 
-        let [, language] = className.split('-');
+    let [, language] = className.split('-');
 
-        if (language === "flutter") {
-            language = "dart";
-        }
-
-        return (
-            <TabItem
-                key={language || index}
-                value={CODE_LANGS[language] || language || index}
-                label={CODE_LANGS[language] ||
-                    <Translate
-                        id='components.CodeExample.NoLanguageTabTitle'
-                        description='The tab title for a code example where no programming language was specified'>
-                        Example
-                    </Translate>
-                }
-                default={defaultVal === CODE_LANGS[language]}
-            >
-                <CodeBlock language={language} showLineNumbers>
-                    {codeProps.children}
-                </CodeBlock>
-            </TabItem>
-        );
-    });
-
-    for (let i = 0; i < WALLET_LANGS.length; i++) {
-        const language = CODE_LANGS[WALLET_LANGS[i]];
-
-        if (tabs.filter((x) => x.props.value === language).length === 0) {
-            if (ALLOW_EMPTY_DOCS) {
-                tabs.push(<TabItem
-                    key={language}
-                    value={language}
-                    label={language}
-                    default={defaultVal === language}
-                >
-                    <CodeBlock language={language} showLineNumbers>
-                        <Translate
-                            id="components.CodeExample.MissingCodeExample"
-                            description='In the Wallet-SDK code example component, this message will display when the selected programming language has no accompanying example'
-                            values={{language: language}}>
-                            {'// There is no code example for {language} yet'}
-                        </Translate>
-                    </CodeBlock>
-                </TabItem>);
-            } else {
-                throw Error(`Missing ${  language  } documentation`);
-            }
-        }
+    if (language === "flutter") {
+      language = "dart";
     }
+
+    return (
+      <TabItem
+        key={language || index}
+        value={CODE_LANGS[language] || language || index}
+        label={CODE_LANGS[language] ||
+          <Translate
+            id='components.CodeExample.NoLanguageTabTitle'
+            description='The tab title for a code example where no programming language was specified'>
+            Example
+          </Translate>
+        }
+        default={defaultVal === CODE_LANGS[language]}
+      >
+        <CodeBlock language={language} showLineNumbers>
+          {codeProps.children}
+        </CodeBlock>
+      </TabItem>
+    );
+  });
+
+  for (let i = 0; i < WALLET_LANGS.length; i++) {
+    const language = CODE_LANGS[WALLET_LANGS[i]];
+
+    if (tabs.filter((x) => x.props.value === language).length === 0) {
+      if (ALLOW_EMPTY_DOCS) {
+        tabs.push(<TabItem
+          key={language}
+          value={language}
+          label={language}
+          default={defaultVal === language}
+        >
+          <CodeBlock language={language} showLineNumbers>
+            <Translate
+              id="components.CodeExample.MissingCodeExample"
+              description='In the Wallet-SDK code example component, this message will display when the selected programming language has no accompanying example'
+              values={{language: language}}>
+              {'// There is no code example for {language} yet'}
+            </Translate>
+          </CodeBlock>
+        </TabItem>);
+      } else {
+        throw Error(`Missing ${  language  } documentation`);
+      }
+    }
+  }
 
    let toShowTabs = tabs;
 
    if (!SHOW_ALL) {
-       for (let i = 0; i < tabs.length; i++) {
-           const language = CODE_LANGS[targetLanguage];
-           if (tabs[i].props.value === language) {
-                toShowTabs = [tabs[i]];
-           }
+     for (let i = 0; i < tabs.length; i++) {
+       const language = CODE_LANGS[targetLanguage];
+       if (tabs[i].props.value === language) {
+        toShowTabs = [tabs[i]];
        }
+     }
    }
 
-    const gid = `wallet-lang${  defaultVal}`;
-    // const gid = "p-wallet" + defaultVal + Math.random()
+  const gid = `wallet-lang${  defaultVal}`;
+  // const gid = "p-wallet" + defaultVal + Math.random()
 
-    return (<Tabs groupId={gid}>
-        {toShowTabs}
-    </Tabs>);
+  return (<Tabs groupId={gid}>
+    {toShowTabs}
+  </Tabs>);
 };
