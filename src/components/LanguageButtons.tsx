8067562c11922a8e07d664bd7d9d8ea46c7a895b
john.xlm <60260750+JFWooten4@users.noreply.github.com>
2026-02-07 02:17:58 -0500

    inline links for readability (#21)
    * fix links to .mdx
    
    * script rendering (fix build)
    
    * fix typefinding consor errors
    
    * use inline image imports
    
    * other pages' direct images
    
    * add meta alt text
    
    * shorten formatted image imports
    
    * inline md page footnotes
    
    * clean removed links section
    goldsky was deleted in https://github.com/stellar/stellar-docs/pull/2055/changes#diff-891f2e3810c11cf187e63b3041e6aa54e068116878224632c2bb02fd09cb5f85
    
    * wierd lab file names
    
    * inline image div widths
    
    * stylize source plaintext hrefs
    
    * same script length display
    
    * other little sourcing nits
    
    * nits on tag linking

--- src/components/LanguageButtons.tsx
+++ src/components/LanguageButtons.tsx
@@ -10,24 +10,24 @@ import Swift from '@site/static/icons/docusaurus/swift.png';
 import SwiftDark from '@site/static/icons/docusaurus/swift-dark.png';
 
 export const LanguageButtons = () => {
-    const {colorMode} = useColorMode();
-    const isDark = colorMode === 'dark';
+  const {colorMode} = useColorMode();
+  const isDark = colorMode === 'dark';
 
-    return (
-        <BrowserOnly>
-            {() =>
-                <div style={{gap: "1rem", display: "flex", marginBottom: "1rem"}}>
-                    <img src={Typescript} onClick={() => setCookie("ts")}  style={{maxHeight: "1.2rem", cursor: "pointer"}}/>
-                    <img src={isDark ? KotlinDark : Kotlin} onClick={() => setCookie("kt")} style={{maxHeight: "1.1rem", cursor: "pointer"}}/>
-                    <img src={isDark ? FlutterDark : Flutter} onClick={() => setCookie("dart")} style={{maxHeight: "1.2rem", cursor: "pointer"}}/>
-                    <img src={isDark ? SwiftDark : Swift} onClick={() => setCookie("swift")} style={{maxHeight: "1.2rem", cursor: "pointer"}}/>
-                </div>
-            }
-        </BrowserOnly>
-    );
+  return (
+    <BrowserOnly>
+      {() =>
+        <div style={{gap: "1rem", display: "flex", marginBottom: "1rem"}}>
+          <img src={Typescript} onClick={() => setCookie("ts")}  style={{maxHeight: "1.2rem", cursor: "pointer"}}/>
+          <img src={isDark ? KotlinDark : Kotlin} onClick={() => setCookie("kt")} style={{maxHeight: "1.1rem", cursor: "pointer"}}/>
+          <img src={isDark ? FlutterDark : Flutter} onClick={() => setCookie("dart")} style={{maxHeight: "1.2rem", cursor: "pointer"}}/>
+          <img src={isDark ? SwiftDark : Swift} onClick={() => setCookie("swift")} style={{maxHeight: "1.2rem", cursor: "pointer"}}/>
+        </div>
+      }
+    </BrowserOnly>
+  );
 };
 
 const setCookie = (str: String) => {
-    document.cookie = `selected_language=${  str  }; SameSite=Strict; expires=Fri, 31 Dec 9999 23:59:59 GMT; Secure`;
-    window.location.reload();
+  document.cookie = `selected_language=${  str  }; SameSite=Strict; expires=Fri, 31 Dec 9999 23:59:59 GMT; Secure`;
+  window.location.reload();
 };
